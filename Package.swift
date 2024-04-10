0bd3f361a227fe1919db9ea8f4eb0bc8a001215d
Leroy Jenkins <build@MiniM1.local>
2024-04-10 11:42:40 -0700

    Updating SDK to beta

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
         .binaryTarget(
             name: "Bugsee",
             url: "https://download.bugsee.com/sdk/ios/spm/Bugsee-beta.zip",
-            checksum: "f45aa032cd1d7ed4a271fe9d7d3dcd80a6581d384ca77b1db8477951e3c99d20")
+            checksum: "309ff0c297de6c73573221db5ab889a6f254116d271f4e5dfa381edb5c479f80")
     ]
 )
